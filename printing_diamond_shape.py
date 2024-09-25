39753efa64ae38cd6f404a456084c0dd6e7b1815
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-09-25 13:15:36 +0800

    Create a function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character.

--- printing_diamond_shape.py
+++ printing_diamond_shape.py
@@ -1,1 +1,10 @@
 # Create a function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character. 
+def print_diamond(n):
+    if n % 2 == 0:
+        return "Please provide an odd integer."
+    
+    for i in range(n):
+        spaces = abs(n // 2 - i)
+        stars = n - 2 * spaces
+        print(" " * spaces + "*" * stars)
+print_diamond(5)
